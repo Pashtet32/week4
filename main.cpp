54461b9ef706751011e5bfd5b167b20883a1f276
Pashtet32 <pavelt0309@gmail.com>
2015-10-18 16:59:40 +0200

    Task2

---
+++ main.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <cstring>
+#include <cstdio>
+#include <cstdlib>
+#include <string>
+
+using namespace std;
+
+float process (char st[])
+{
+    char st2[256];
+    strcpy(st2, st);
+    char *num = strtok(st2, "+-");
+    float res;
+   // cout << num<< endl;
+    res = atof(num);
+    for (int i  = 0; i < strlen(st); i++)
+    {
+        if (st[i] == '+')
+        {
+          //  cout << num << endl;
+            num = strtok(NULL, "+-");
+            res += atof(num);
+        }
+        if (st[i] == '-')
+        {
+          //  cout << num << endl;
+            num = strtok(NULL, "+-");
+            res += atof(num);
+        }
+    }
+
+    return res;
+}
+
+int main()
+{
+    char st[256];
+    cin.getline(st, 256);
+    cout << process(st);
+    return 0;
+}
