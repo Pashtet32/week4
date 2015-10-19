fad5a628dc74fd922cbd7ca34fa093621f3221a2
dmitriyomelyusik <omelya.emelya@gmail.com>
2015-10-19 17:23:29 +0300

    Task1.Test

--- main.cpp
+++ main.cpp
@@ -30,10 +30,28 @@ float process (char st[])
     return res;
 }
 
+void assert(char ch[], float res)
+{
+    if (process(ch) == res)
+        cout << "ok" << endl;
+    else
+        cout << "very bad" << endl;
+}
+
 int main()
 {
-    char st[256];
-    cin.getline(st, 256);
-    cout << process(st);
+//    char st[256];
+//    cin.getline(st, 256);
+//    cout << process(st);
+    assert ("1+1", 2);
+    assert ("1+1-10 +  1008", 1000);
+    assert ("1+1 - - 1", 1);
+    assert ("11 - 11 + 22", 22);
+    assert ("1-11", -10);
+    assert ("1 - 2 + 3 - 4 + 5 - 6 + 7", 4);
+    assert ("1 - 1 + 1  - 1 + 1 -1", 0);
+    assert ("134", 134);
+    assert ("111-222", -111);
+    assert ("1-1", 0);
     return 0;
 }
